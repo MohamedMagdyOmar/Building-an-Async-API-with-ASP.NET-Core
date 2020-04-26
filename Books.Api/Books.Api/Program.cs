ce163922b59ec7231666f0be93574a02de7dc7d9
MohamedMagdyOmar <eng_mohamed_magdy@hotmail.com>
2020-04-26 15:55:15 -0400

    Demo Starting from Scratch with a DAL

--- Books.Api/Books.Api/Program.cs
+++ Books.Api/Books.Api/Program.cs
@@ -3,9 +3,12 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
+using Books.Api.Contexts;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 
 namespace Books.Api
@@ -14,7 +17,24 @@ namespace Books.Api
     {
         public static void Main(string[] args)
         {
-            CreateWebHostBuilder(args).Build().Run();
+            var host = CreateWebHostBuilder(args).Build();
+
+            using (var scope = host.Services.CreateScope())
+            {
+                try
+                {
+                    // here we need to make sure that migration is executed when our application starts
+                    var context = scope.ServiceProvider.GetService<BooksContext>();
+                    context.Database.Migrate();
+                }
+
+                catch(Exception e)
+                {
+                    var logger = scope.ServiceProvider.GetRequiredService<ILogger<Program>>();
+                    logger.LogError(e, "An error occured while migrating the database.");
+                }
+
+            }
         }
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
