4c90a40f678ef8cf55cc033db3e84935475ca173
MohamedMagdyOmar <eng_mohamed_magdy@hotmail.com>
2020-05-05 13:03:43 -0400

    Supporting Bulk Inserts

---
+++ Books.Api/Books.Api/Controllers/BooksCollectionController.cs
@@ -0,0 +1,39 @@
+﻿using AutoMapper;
+using Books.Api.Models;
+using Books.Api.Services;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Books.Api.Controllers
+{
+    [Route("api/[Controller]")]
+    [ApiController]
+    public class BooksCollectionController : ControllerBase
+    {
+        private IBooksRepository _booksRepository;
+        private IMapper _mapper;
+
+        public BooksCollectionController(IBooksRepository booksRepository, IMapper mapper)
+        {
+            _booksRepository = booksRepository?? throw new ArgumentException(nameof(booksRepository));
+            _mapper = mapper?? throw new ArgumentException(nameof(mapper));
+        }
+
+        public async Task<IActionResult> CreateBookCollection([FromBody] IEnumerable<BookForCreation> bookCollection)
+        {
+            var bookEntities = _mapper.Map<IEnumerable<Entities.Book>>(bookCollection);
+            
+            foreach(var book in bookEntities)
+            {
+                _booksRepository.AddBook(book);
+            }
+
+           await _booksRepository.SaveChangesAsync();
+
+            return Ok();
+        }
+    }
+}
