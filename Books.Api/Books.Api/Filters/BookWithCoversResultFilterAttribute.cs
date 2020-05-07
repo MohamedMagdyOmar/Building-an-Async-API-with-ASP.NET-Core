c0c7bfe6c729885cb59d187bbbeb8321edc9884e
MohamedMagdyOmar <eng_mohamed_magdy@hotmail.com>
2020-05-07 12:22:35 -0400

    Passing Multiple Objects to an AsyncResultFilter with ValueTuple

---
+++ Books.Api/Books.Api/Filters/BookWithCoversResultFilterAttribute.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Books.Api.Filters
+{
+    public class BookWithCoversResultFilterAttribute : ResultFilterAttribute
+    {
+        public override async Task OnResultExecutionAsync(ResultExecutingContext context, ResultExecutionDelegate next)
+        {
+            var resultFromAction = context.Result as ObjectResult;
+            if(resultFromAction?.Value == null || resultFromAction.StatusCode < 200 || resultFromAction.StatusCode >= 300)
+            {
+                await next();
+                return;
+            }
+
+            //var (book, bookCovers) = ((Entities.Book book, IEnumerable<ExternalModels.BookCover> bookCovers))resultFromAction.Value;
+
+            // just nicer way than previous one
+            var (book, bookCovers) = ((Entities.Book, IEnumerable<ExternalModels.BookCover>))resultFromAction.Value;
+
+            await next();
+        }
+    }
+}
