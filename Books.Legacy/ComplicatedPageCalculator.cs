9c32fc7910cdb3929f0d168239482239c7b66c7b
MohamedMagdyOmar <eng_mohamed_magdy@hotmail.com>
2020-05-07 15:02:24 -0400

    Wrapping Synchronous Code with Task.Run()

---
+++ Books.Legacy/ComplicatedPageCalculator.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading;
+
+namespace Books.Legacy
+{
+    public class ComplicatedPageCalculator 
+    {
+        /// <summary>
+        /// Full CPU load for 5 seconds
+        /// </summary>
+        public int CalculateBookPages()
+        {
+            // this method simulates that we calculate number of pages in a book
+            // this method is legacy "Non Async" code that is long running on the CPU Thread
+            var watch = new Stopwatch();
+            watch.Start();
+            while (true)
+            {
+                if (watch.ElapsedMilliseconds > 5000)
+                {
+                    break;
+                } 
+            }
+
+            return 42;
+        } 
+    }
+}
